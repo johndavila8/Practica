a39b3b55abfa7d1b2e39ca549bba14025a8a24f3
PC-John <PC-John@10.164.50.83>
2019-09-24 08:39:08 +0200

    Creada estructura del proyecto

---
+++ SEGUNDO 1º/src/ej1Ventana.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+
+public class ej1Ventana extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	
+	public ej1Ventana() {
+		System.out.println();
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setTitle("Cerrar la ventana");
+		this.setSize(300,200);
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		ej1Ventana ej1Ventana = new ej1Ventana();
+		ej1Ventana.setVisible(true);
+	}
+}
